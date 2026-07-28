b7abdb3884b61a9fda00f5a7c05bf166d346af84
Rehaan Ahmad <rehaanahmad2013@gmail.com>
2026-07-28 12:32:17 -0700

    configure full reproduction seed 271828

--- reproduction/config.sh
+++ reproduction/config.sh
@@ -1,5 +1,5 @@
 # Experiment knobs live in committed code; the orx run command is invariant.
-MODE="scan"
-SEED="20260728"
+MODE="full"
+SEED="271828"
 TRIALS="30000"
 SIM_NS="1,2,3,5,8,13,21,34,55,89"
